1dbaccf8a695f4184f3b086a0139a0d621bf8c0f
sirpolux <poluxreloaded@gmail.com>
2024-01-23 10:02:43 +0100

    Boiler Plate Setup

---
+++ src/main/java/com/cluster2/ajosavingscluster2/AjoSavingsCluster2Application.java
@@ -0,0 +1,13 @@
+package com.cluster2.ajosavingscluster2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjoSavingsCluster2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjoSavingsCluster2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cluster2/ajosavingscluster2/AjoSavingsCluster2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluster2.ajosavingscluster2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjoSavingsCluster2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
